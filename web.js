3ba6f7536368a81fa443cef4eaea22e293b7e9b4
Jeremy Merrifield <nofear5599@gmail.com>
2013-07-09 17:55:24 +0000

    changed to show index html

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs =  require('fs');
 
-var index = fs.readFile('index.html');
-var buf = new Buffer(index);
+var index = fs.readFileSync('index.html','utf8');
+var buf = new Buffer(index,'utf8');
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf8'));
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
